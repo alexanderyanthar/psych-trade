2578ef8aa88d6e38de94f92c18e2f4e958f7d3c0
alexanderyanthar <alexander.yanthar@outlook.com>
2023-08-05 13:16:26 -0400

    created basic user authenticaion with user info logged to mongoDB

--- backend/app.mjs
+++ backend/app.mjs
@@ -18,6 +18,9 @@ app.use(session({
     saveUninitialized: false,
 }));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -56,13 +59,39 @@ passport.deserializeUser((id, done) => {
 app.post('/login', passport.authenticate('local', { successRedirect: '/profile', failureRedirect: '/login'}));
 
 app.get('/profile', (req, res) => {
-    res.send(`Welcome, ${req.user.username}!`);
+    res.json(`Welcome, ${req.user.username}!`);
 });
 
 app.get('/logout', (req, res) => {
     req.logOut();
     res.redirect('/');
 });
+
+// Add a new route for handling signup
+app.post('/signup', async (req, res) => {
+    const { username, password } = req.body;
+    res.send(`got the username: ${username} and pass ${password}`);
+    try {
+        const { username, password } = req.body;
+
+        const existingUser = await User.findOne({ username });
+        if(existingUser) {
+            return res.status(400).send('Username already taken');
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({ username, password: hashedPassword });
+        await newUser.save();
+
+        console.log('got it');
+        res.redirect('/login');
+    } catch(err) {
+        console.error(err);
+        res.status(500).send('Error creating user');
+    }
+});
+
 
 app.listen(PORT,() => {
     console.log(`Server listening on port ${PORT}`);
